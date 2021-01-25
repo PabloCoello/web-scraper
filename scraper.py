915ffeed8fa9d29f3425f6c04d9e1f4580fc46fe
Pablo Coello <pablo.coello@outlook.com>
2021-01-25 22:57:14 +0100

    Added comments

--- scraper.py
+++ scraper.py
@@ -8,9 +8,33 @@ import pandas as pd
 
 class Scraper:
     '''
+    Class aimed to retrieve data directly from websites.
     '''
     def __init__(self, path, buttons, tables):
         '''
+        args:
+            -path: string, path to the website where de data is.
+
+            -buttons: dict, buttons to be pressed in the website.
+                      This dict has to have the following structure:
+                      {
+                          'name_of_the_button1': [button_type, xpath]
+                      }
+                      name_of_the_button: can be any, allows quick access to the 
+                                          desired button
+                      button_type: Currently only implemented "select" buttons
+                      xpath: xpath of the button taken from the website
+            
+            -tables: dict, information about the table to be accesed.
+                     This dict has to have the following structure:
+                     {
+                         'name_of_the_table': [number_of_columns, xpath]
+                     }
+                     name_of_the_table: can be any, allows quick access to the 
+                                        desired table
+                     number_of_columns: number of columns opf the table to be parsed
+                     xpath: xpath of the button taken from the website
+
         '''
         options = Options()
         options.headless = True
@@ -21,6 +45,11 @@ class Scraper:
 
     def presh_button(self, name, value):
         '''
+        Method for pressing any button in the website.
+
+        args:
+            -name: string, name of the button (key in buttons dict)
+            -value: string, value to be selected
         '''
         if self.buttons[name][0] == 'select':
             self.select_button(
@@ -30,12 +59,21 @@ class Scraper:
 
     def select_button(self, xpath, value):
         '''
+        Specific method for select buttons.
+
+        args:
+            -xpath: string, xpath to the button
+            -value: string, value to be selected
         '''
         name = Select(self.driver.find_element_by_xpath(xpath))
         name.select_by_value(value)
 
     def get_table(self, name):
         '''
+        Retrieves data from a table as df.
+
+        args:
+            -name: string, name of the table
         '''
         table = self.driver.find_element_by_xpath(self.tables[name][1])
 
@@ -61,11 +99,14 @@ class Scraper:
 
     def close_client(self):
         '''
+        Closes connection.
         '''
         self.driver.quit()
 
 
 if __name__ == '__main__':
+    # Example 1:
+
     path = 'http://www.intecmar.gal/Informacion/biotoxinas/Evolucion/CierresBatea.aspx'
     buttons = {
         'toxicidade': ['select','//*[@id="ctl00_Contenido_dpToxina2"]'],
